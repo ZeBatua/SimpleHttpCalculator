a39e0cead565eac33d8fe08c20d11298dda12d81
thalassa_home <blinov.gosha@mail.ru>
2023-05-01 22:35:46 +0300

    Initial commit

---
+++ src/main/java/simple/http/calculator/controlers/FirstController.java
@@ -0,0 +1,55 @@
+package simple.http.calculator.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/app")
+public class FirstController {
+    @GetMapping("/calc")
+    public String makeCalc(@RequestParam(value = "first", required = false) Double first,
+                           @RequestParam(value = "second", required = false) Double second,
+                           @RequestParam(value = "sign", required = false) String sign,
+                           Model model) {
+        if (checkValues(first, second, sign)) {
+            double result = calcValues(first, second, sign);
+            if (sign.equals(" ")) sign = "+";
+            model.addAttribute("message", first + " " + sign + " " + second + " = " + result);
+            return "app/calc";
+        } else {
+            model.addAttribute("message", "Invalid values or sign");
+            return  "app/error";
+        }
+
+    }
+
+    private double calcValues(Double first, Double second, String sign) {
+        double result = 0.0;
+        if (sign.equals(" ") || sign.equals("+")) { // +
+            result = first + second;
+        } else if (sign.equals("-")) {
+            result = first - second;
+        } else if (sign.equals("*")) {
+            result = first * second;
+        } else if (sign.equals("/")) {
+            result = first / second;
+        }
+        return result;
+    }
+
+    private boolean checkValues(Double v1, Double v2, String sign) {
+        if (v1 != null && v2 != null && isSign(sign)) return true;
+        else return false;
+            
+    }
+
+    private boolean isSign(String sign) {
+        if (sign != null && (sign.equals("+") ||sign.equals(" ") || sign.equals("-") || sign.equals("*") || sign.equals("/"))) return true;
+        else return false;
+    }
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hi, this is a simple calculator. To enter the values, use the link above.<br />
+    The values must be entered as follows:<br />
+    /app/calc?first=20.5&second=20.5&sign=*<br />
+    Operations are currently allowed "+", "-", "*", "/".</h2>
+<a href="/app/calc?first=20.5&second=20.5&sign=+">Example request with parameters</a>
+</body>
+</html>
